ecf132a462a5ff50afe557bab7e4a387595cbe94
Julia Torres Bolshaw Salles <Julia.SALLES@student.tafesa.edu.au>
2025-09-08 13:02:13 +0930

    Commit the branch feat_wrap1.1

--- ProductMaintenance/ProductApps/MainWindow.xaml.cs
+++ ProductMaintenance/ProductApps/MainWindow.xaml.cs
@@ -36,6 +36,8 @@ namespace ProductApps
                 totalPaymentTextBlock.Text = Convert.ToString(cProduct.TotalPayment);
 
                 totalChargeTextBlock.Text = Convert.ToString(cProduct.TotalPayment + 25);
+
+                totalChargeWrapTextBlock.Text = Convert.ToString(cProduct.TotalPayment + 25 + 5);
             }
             catch (FormatException)
             {
